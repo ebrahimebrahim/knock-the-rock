b50e156f72f3c7c552db128302ccafd08db0d751
Yusuf Ebrahim <yusufmte@gmail.com>
2020-09-10 14:23:15 -0400

    randomly generated rocks now work (generate_vertices() in Rock.gd)

--- ktr/Rock.gd
+++ ktr/Rock.gd
@@ -1,13 +1,12 @@
 extends RigidBody2D
 
-
 var vertices : PoolVector2Array
 var color = Color(0.6, 0.6, 0.6)
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	vertices = PoolVector2Array([Vector2(-20,-10),Vector2(0,10),Vector2(20,-10),Vector2(0,-100)])
+	randomize() # <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ this was added so that it's not the SAME random number every time
+	vertices = generate_vertices()
 	
 	var collision_shape : CollisionShape2D = CollisionShape2D.new()
 	add_child(collision_shape)
@@ -17,11 +16,26 @@ func _ready():
 	
 	collision_shape.shape.points =  vertices
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame. <~~~~~~~~~~~~~~~~~~~~ can we get rid of this?
 #func _process(delta):
 #	pass
 
+func generate_vertices():
+	var r = rand_range(10,50)
+	var n = rand_range(3,15)
+	var dt = 2*PI/n
+	var verts = PoolVector2Array()
+	var mini_r = r*sin(dt/2)
+	var mini_c : Vector2
+	var new_vert = Vector2(0,0)
+	for i in range(n):
+		mini_c = Vector2(r*cos(dt*i),r*sin(dt*i))
+		while(new_vert.distance_to(mini_c) >= mini_r):
+			new_vert = Vector2(rand_range(mini_c.x-mini_r,mini_c.x+mini_r),rand_range(mini_c.y-mini_r,mini_c.y+mini_r))
+		verts.push_back(new_vert)
+	return verts
+
 func _draw():
-	draw_polygon(vertices,[color])
+	draw_polygon(vertices, [color])
 	draw_polyline(vertices,Color(0.0,0.0,0.0),2.0,true)
 	draw_line(vertices[-1],vertices[0],Color(0.0,0.0,0.0),2.0,true)
