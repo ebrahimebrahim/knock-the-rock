da50cc75808a7123acf3474e8d3924eb4bb48263
Yusuf Ebrahim <yusufmte@gmail.com>
2020-09-10 15:14:13 -0400

    added random stretching and rotation to generate_vertices

--- ktr/Rock.gd
+++ ktr/Rock.gd
@@ -5,7 +5,6 @@ var color = Color(0.6, 0.6, 0.6)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize() # <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ this was added so that it's not the SAME random number every time
 	vertices = generate_vertices()
 	
 	var collision_shape : CollisionShape2D = CollisionShape2D.new()
@@ -16,23 +15,24 @@ func _ready():
 	
 	collision_shape.shape.points =  vertices
 
-# Called every frame. 'delta' is the elapsed time since the previous frame. <~~~~~~~~~~~~~~~~~~~~ can we get rid of this?
-#func _process(delta):
-#	pass
-
 func generate_vertices():
-	var r = rand_range(10,50)
-	var n = rand_range(3,15)
+	randomize()
+	var r = rand_range(20,45)
+	var n = rand_range(5,15)
 	var dt = 2*PI/n
 	var verts = PoolVector2Array()
 	var mini_r = r*sin(dt/2)
-	var mini_c : Vector2
 	var new_vert = Vector2(0,0)
 	for i in range(n):
-		mini_c = Vector2(r*cos(dt*i),r*sin(dt*i))
+		var mini_c = Vector2(r*cos(dt*i),r*sin(dt*i))
 		while(new_vert.distance_to(mini_c) >= mini_r):
 			new_vert = Vector2(rand_range(mini_c.x-mini_r,mini_c.x+mini_r),rand_range(mini_c.y-mini_r,mini_c.y+mini_r))
 		verts.push_back(new_vert)
+	var stretch = exp(rand_range(-0.5,0.5))
+	var rot = rand_range(0,2*PI)
+	for i in range(len(verts)):
+		verts[i].x *= stretch
+		verts[i] = verts[i].rotated(rot)
 	return verts
 
 func _draw():
