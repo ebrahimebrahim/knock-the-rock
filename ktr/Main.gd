8a31ae87fc107eda5c6b70a0cad5c17c55cf93c4
Yusuf Ebrahim <yusufmte@gmail.com>
2020-09-12 06:05:28 -0400

    put the rock putting in Main into a function

--- ktr/Main.gd
+++ ktr/Main.gd
@@ -4,7 +4,10 @@ const Rock = preload("res://Rock.gd")
 
 func _ready():
 	randomize()
-	for _i in range(4):
-		var rock : Rock = Rock.new()
-		add_child(rock)
-		rock.set_position(Vector2(rand_range(100,900),rand_range(0,300)))
+	put_rock(4,Vector2(rand_range(100,900),rand_range(0,300)))
+
+func put_rock(num : int, pos : Vector2) -> void:
+		for _i in range(num):
+			var rock : Rock = Rock.new()
+			add_child(rock)
+			rock.set_position(pos)
