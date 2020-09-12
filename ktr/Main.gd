52693821d6e8ae70a74f197c532992069af153d1
Yusuf Ebrahim <yusufmte@gmail.com>
2020-09-12 06:09:13 -0400

    fixed rock putting function in Main

--- ktr/Main.gd
+++ ktr/Main.gd
@@ -4,10 +4,11 @@ const Rock = preload("res://Rock.gd")
 
 func _ready():
 	randomize()
-	put_rock(4,Vector2(rand_range(100,900),rand_range(0,300)))
+	put_rock(4)
 
-func put_rock(num : int, pos : Vector2) -> void:
+func put_rock(num : int, pos = Vector2(0,0)) -> void:
 		for _i in range(num):
 			var rock : Rock = Rock.new()
 			add_child(rock)
-			rock.set_position(pos)
+			if pos == Vector2(0,0): rock.set_position(Vector2(rand_range(100,900),rand_range(0,300)))
+			else: rock.set_position(pos)
