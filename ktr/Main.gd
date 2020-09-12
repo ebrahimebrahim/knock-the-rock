becc841e86ef71e332cbe734f3db27cd02a14dc3
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-12 10:52:51 -0400

    use null as default argument in put_rock

--- ktr/Main.gd
+++ ktr/Main.gd
@@ -8,16 +8,16 @@ func _ready():
 	put_rock(8)
 	put_boulder(1,Vector2(500,300))
 
-func put_rock(num : int, pos = Vector2(0,0)) -> void:
+func put_rock(num : int, pos = null) -> void:
 		for _i in range(num):
 			var rock : Rock = Rock.new()
 			add_child(rock)
-			if pos == Vector2(0,0): rock.set_position(Vector2(rand_range(100,900),rand_range(0,300)))
+			if pos == null: rock.set_position(Vector2(rand_range(100,900),rand_range(0,300)))
 			else: rock.set_position(pos)
 
-func put_boulder(num : int, pos = Vector2(0,0)) -> void:
+func put_boulder(num : int, pos = null) -> void:
 		for _i in range(num):
 			var beuld : Boulder = Boulder.new()
 			add_child(beuld)
-			if pos == Vector2(0,0): beuld.set_position(Vector2(rand_range(100,900),rand_range(0,300)))
+			if pos == null: beuld.set_position(Vector2(rand_range(100,900),rand_range(0,300)))
 			else: beuld.set_position(pos)
