47ca960ef8d7ebcb7ed13527d068570557032faa
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-12 10:53:22 -0400

    simplify put_boulder
    since it never needs to be randomized

--- ktr/Main.gd
+++ ktr/Main.gd
@@ -6,7 +6,7 @@ const Boulder = preload("res://Boulder.gd")
 func _ready():
 	randomize()
 	put_rock(8)
-	put_boulder(1,Vector2(500,300))
+	put_boulder(Vector2(500,300))
 
 func put_rock(num : int, pos = null) -> void:
 		for _i in range(num):
@@ -15,9 +15,7 @@ func put_rock(num : int, pos = null) -> void:
 			if pos == null: rock.set_position(Vector2(rand_range(100,900),rand_range(0,300)))
 			else: rock.set_position(pos)
 
-func put_boulder(num : int, pos = null) -> void:
-		for _i in range(num):
-			var beuld : Boulder = Boulder.new()
-			add_child(beuld)
-			if pos == null: beuld.set_position(Vector2(rand_range(100,900),rand_range(0,300)))
-			else: beuld.set_position(pos)
+func put_boulder(pos : Vector2) -> void:
+		var beuld : Boulder = Boulder.new()
+		add_child(beuld)
+		beuld.set_position(pos)
