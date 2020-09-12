a6a4e79271a7f6cbfbffdefb6737bf7266dc650e
Yusuf Ebrahim <yusufmte@gmail.com>
2020-09-12 04:04:55 -0400

    removed no longer needed files

--- ktr/AudioStreamPlayer2D.gd
+++
@@ -1,8 +0,0 @@
-extends AudioStreamPlayer2D
-
-
-func _on_knock(impact_velocity,impact_pos,lighter_mass):
-	position = impact_pos
-	volume_db = min((impact_velocity - 200)/50  -  20 , 18)
-	pitch_scale = exp(-lighter_mass/1864  +  0.941944)
-	play()
