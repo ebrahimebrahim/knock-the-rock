e3c2094e326ca3bedba2420a0c54f0dc0bd1d054
Yusuf Ebrahim <yusufmte@gmail.com>
2020-09-15 03:18:39 -0400

    wrote functions for HUD buttons (all except "Help") and renamed "close HUD" to "toggle HUD"

--- ktr/HUDZone.gd
+++ ktr/HUDZone.gd
@@ -10,16 +10,17 @@ func _input(event):
 	if event.is_action_pressed("return_to_menu"): _on_return()
 	if event.is_action_pressed("restart"): _on_restart()
 	if event.is_action_pressed("open_help"): _on_help()
-	if event.is_action_pressed("close_hud"): _on_close()
+	if event.is_action_pressed("toggle_hud"): _on_toggle()
 
 func _on_return():
-	print("return peup")
+	get_tree().change_scene("res://MenuScreen.tscn")
 
 func _on_restart():
-	print("restart peup")
+	get_tree().reload_current_scene()
 
 func _on_help():
 	print("help peup")
 
-func _on_close():
-	print("close peup")
+func _on_toggle():
+	if visible: hide()
+	else: show()
