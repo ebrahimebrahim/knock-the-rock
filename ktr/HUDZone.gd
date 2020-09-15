6d26a08cd42514ca0520eba6a19d127478612351
Yusuf Ebrahim <yusufmte@gmail.com>
2020-09-15 03:14:02 -0400

    connected keypress actions to placeholder functions for ingame HUD

--- ktr/HUDZone.gd
+++ ktr/HUDZone.gd
@@ -6,14 +6,20 @@ func _ready():
 	$RestartButton.connect("button_up",self,"_on_restart")
 	$HelpButton.connect("button_up",self,"_on_help")
 
+func _input(event):
+	if event.is_action_pressed("return_to_menu"): _on_return()
+	if event.is_action_pressed("restart"): _on_restart()
+	if event.is_action_pressed("open_help"): _on_help()
+	if event.is_action_pressed("close_hud"): _on_close()
+
 func _on_return():
-	pass
+	print("return peup")
 
 func _on_restart():
-	pass
+	print("restart peup")
 
 func _on_help():
-	pass
+	print("help peup")
 
 func _on_close():
-	pass
+	print("close peup")
