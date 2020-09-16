a115f69cbedb8d922dfb451ce31d53a6704a68c8
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-16 11:11:07 -0400

    separate drawing and physics aspects of Rocks
    This commit breaks boulders! They will be fixed soon
    For some reason this won't work if we make RockPolygon.gd extend CanvasItem
    It only works if we extend Node2D
    I don't see why. It seems like CanvasItem is what we really want since
    a RockPolygon should not be allowed to have its own position/rotation/scale--
    it should derive that from its parent

--- ktr/Rock.gd
+++ ktr/Rock.gd
@@ -1,15 +1,8 @@
 extends RigidBody2D
 
-var vertices = PoolVector2Array()
-var texture_uvs = PoolVector2Array()
-var texture : ImageTexture
+const RockPolygon = preload("res://RockPolygon.gd")
 
-const colors = [Color("565656"),
-				Color("ffffff"),
-				Color("635353"),
-				Color("6f5e3f"),
-				Color("7d7e57")]
-
+var rock_polygon : RockPolygon
 var is_held : bool = false setget set_held
 var local_hold_point : Vector2
 
@@ -23,10 +16,10 @@ var audio_timer : Timer
 
 
 func _init():
-	generate_texture()
-	self_modulate = generate_color()
-	generate_polygon()
-	mass = pow(calculate_area()/1000,1.5)
+	rock_polygon = RockPolygon.new()
+	add_child(rock_polygon)
+	
+	mass = pow(rock_polygon.calculate_area()/1000,1.5)
 	
 	continuous_cd = CCD_MODE_CAST_RAY # might make this an option
 	var phys = PhysicsMaterial.new()
@@ -51,78 +44,18 @@ func _init():
 	audio_timer.one_shot = true
 	audio_timer.wait_time = 0.2 # between audio clips
 
-func generate_texture():
-	texture = ImageTexture.new()
-	var noise = OpenSimplexNoise.new()
-	noise.seed = randi()
-	noise.octaves = 6
-	noise.period = 256
-	noise.persistence = 1
-	noise.lacunarity = 2.54
-	var img = noise.get_image(32,32)
-	rockify_image(img)
-	texture.create_from_image(img)
-	texture.set_flags(texture.FLAGS_DEFAULT & ~(texture.FLAG_REPEAT))
-
-
-func rockify_image(img : Image) -> void:
-	img.convert(img.FORMAT_L8) # convert to grayscale
-	for i in range(len(img.data["data"])):
-		img.data["data"][i] = (img.data["data"][i]/32)*32
-
-
-func generate_color() -> Color:
-	var output_color = colors[randi()%colors.size()]
-	for _i in range(3):
-		output_color = output_color.linear_interpolate(colors[randi()%colors.size()],randf())
-	return output_color
-
-
-func random_point_in_disk(center : Vector2, radius : float):
-	var output = center+100*Vector2(radius,0)
-	while(output.distance_to(center) >= radius):
-		output = Vector2(rand_range(center.x-radius,center.x+radius),rand_range(center.y-radius,center.y+radius))
-	return output
-
-
-func generate_polygon():
-	var r = rand_range(20,45)
-	var n = 5 + randi()%11
-	var dt = 2*PI/n
-	var mini_r = r*sin(dt/2)
-	var stretch = exp(rand_range(-0.5,0.5))
-	var rot = rand_range(0,2*PI)
-	for i in range(n):
-		var new_vert = random_point_in_disk(r*Vector2(cos(dt*i),sin(dt*i)),mini_r)
-		new_vert.x *= stretch
-		new_vert = new_vert.rotated(rot)
-		vertices.push_back(new_vert)
-		
-		texture_uvs.push_back(0.5*(Vector2(cos(dt*i),sin(dt*i)) + Vector2(1,1)))
-
-
-func calculate_area():
-	var area = 0
-	for i in range(len(vertices)):
-		area += vertices[i].cross(vertices[(i+1)%len(vertices)])/2
-	return area
-
 
 func generate_collision_shape():
-	var triangles : PoolIntArray = Geometry.triangulate_polygon(vertices)
+	var triangles : PoolIntArray = Geometry.triangulate_polygon(rock_polygon.vertices)
 	assert(len(triangles)%3==0)
 	for i in range(len(triangles)/3):
-		var triangle = PoolVector2Array([vertices[triangles[3*i]],vertices[triangles[3*i+1]],vertices[triangles[3*i+2]]])
+		var triangle = PoolVector2Array([rock_polygon.vertices[triangles[3*i]],
+										 rock_polygon.vertices[triangles[3*i+1]],
+										 rock_polygon.vertices[triangles[3*i+2]]])
 		var c = CollisionShape2D.new()
 		add_child(c)
 		c.shape = ConvexPolygonShape2D.new()
 		c.shape.points = triangle
-
-
-func _draw():
-	draw_polygon(vertices, [], texture_uvs, texture)
-	draw_polyline(vertices,Color(0.0,0.0,0.0),2.0,true)
-	draw_line(vertices[-1],vertices[0],Color(0.0,0.0,0.0),2.0,true)
 
 
 func _process(delta):
@@ -143,7 +76,7 @@ func _on_input(event):
 	# on click, hold rock
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
 		var vertices_global = PoolVector2Array()
-		for v in vertices:
+		for v in rock_polygon.vertices:
 			vertices_global.push_back(global_transform.xform(v))
 		if Geometry.is_point_in_polygon(event.position,vertices_global):
 			set_held(true)
@@ -181,43 +114,27 @@ func knock(impact_vel : float, lighter_mass : float, knock_type):
 # Return vertical distance from rock origin to that flat edge
 func flat_bottom() -> float:
 	var edge_lengths = []
-	for i in range(len(vertices)):
-		edge_lengths.push_back((vertices[i] - vertices[(i+1)%len(vertices)]).length_squared())
+	for i in range(len(rock_polygon.vertices)):
+		edge_lengths.push_back((rock_polygon.vertices[i] - rock_polygon.vertices[(i+1)%len(rock_polygon.vertices)]).length_squared())
 	var max_edge_length : float = edge_lengths.max()
 	var argmax : int = edge_lengths.find(max_edge_length)
-	var v : Vector2 = vertices[argmax] - vertices[(argmax+1)%len(vertices)]
+	var v : Vector2 = rock_polygon.vertices[argmax] - rock_polygon.vertices[(argmax+1)%len(rock_polygon.vertices)]
 	rotation = -v.angle()
 	
 	var u : Vector2 = v.normalized()
-	return (vertices[argmax] - vertices[argmax].dot(u)*u).length()
-	
-
-# Returns the center of mass, aka centroid, of the polygon in local coords
-func center_of_mass() -> Vector2:
-	var x : float = 0
-	var y : float = 0
-	for i in range(len(vertices)):
-		var vi = vertices[i]
-		var vi1 = vertices[(i+1)%len(vertices)]
-		x += (vi.x + vi1.x)*(vi.x * vi1.y - vi1.x * vi.y)
-		y += (vi.y + vi1.y)*(vi.x * vi1.y - vi1.x * vi.y) 
-	return 1/(6 * calculate_area()) * Vector2(x,y)
+	return (rock_polygon.vertices[argmax] - rock_polygon.vertices[argmax].dot(u)*u).length()
 
 
 # Returns the global coords of the vertex that is leftmost in global coords
 func leftmost_vertex() -> Vector2:
-	var xs = []
-	for v in vertices:
-		xs.push_back(global_transform.xform(v).x)
-	var argmin = xs.find(xs.min())
-	return global_transform.xform(vertices[argmin])
+	return rock_polygon.leftmost_vertex()
 
 
 # Returns the global coords of the vertex that is rightmost in global coords
 func rightmost_vertex() -> Vector2:
-	var xs = []
-	for v in vertices:
-		xs.push_back(global_transform.xform(v).x)
-	var argmax = xs.find(xs.max())
-	return global_transform.xform(vertices[argmax])
+	return rock_polygon.rightmost_vertex()
 
+
+# Returns the center of mass, aka centroid, of the rock polygon in local coords
+func center_of_mass() -> Vector2:
+	return rock_polygon.center_of_mass()
---
+++ ktr/RockPolygon.gd
@@ -0,0 +1,111 @@
+extends Node2D
+
+var vertices = PoolVector2Array()
+var texture_uvs = PoolVector2Array()
+var texture : ImageTexture
+
+const colors = [Color("565656"),
+				Color("ffffff"),
+				Color("635353"),
+				Color("6f5e3f"),
+				Color("7d7e57")]
+
+
+func _init():
+	generate_texture()
+	self_modulate = generate_color()
+	generate_polygon()
+	
+
+func generate_texture():
+	texture = ImageTexture.new()
+	var noise = OpenSimplexNoise.new()
+	noise.seed = randi()
+	noise.octaves = 6
+	noise.period = 256
+	noise.persistence = 1
+	noise.lacunarity = 2.54
+	var img = noise.get_image(32,32)
+	rockify_image(img)
+	texture.create_from_image(img)
+	texture.set_flags(texture.FLAGS_DEFAULT & ~(texture.FLAG_REPEAT))
+
+
+func rockify_image(img : Image) -> void:
+	img.convert(img.FORMAT_L8) # convert to grayscale
+	for i in range(len(img.data["data"])):
+		img.data["data"][i] = (img.data["data"][i]/32)*32
+
+
+func generate_color() -> Color:
+	var output_color = colors[randi()%colors.size()]
+	for _i in range(3):
+		output_color = output_color.linear_interpolate(colors[randi()%colors.size()],randf())
+	return output_color
+
+
+func random_point_in_disk(center : Vector2, radius : float):
+	var output = center+100*Vector2(radius,0)
+	while(output.distance_to(center) >= radius):
+		output = Vector2(rand_range(center.x-radius,center.x+radius),rand_range(center.y-radius,center.y+radius))
+	return output
+
+
+func generate_polygon():
+	var r = rand_range(20,45)
+	var n = 5 + randi()%11
+	var dt = 2*PI/n
+	var mini_r = r*sin(dt/2)
+	var stretch = exp(rand_range(-0.5,0.5))
+	var rot = rand_range(0,2*PI)
+	for i in range(n):
+		var new_vert = random_point_in_disk(r*Vector2(cos(dt*i),sin(dt*i)),mini_r)
+		new_vert.x *= stretch
+		new_vert = new_vert.rotated(rot)
+		vertices.push_back(new_vert)
+		
+		texture_uvs.push_back(0.5*(Vector2(cos(dt*i),sin(dt*i)) + Vector2(1,1)))
+
+
+func calculate_area():
+	var area = 0
+	for i in range(len(vertices)):
+		area += vertices[i].cross(vertices[(i+1)%len(vertices)])/2
+	return area
+
+
+func _draw():
+	draw_polygon(vertices, [], texture_uvs, texture)
+	draw_polyline(vertices,Color(0.0,0.0,0.0),2.0,true)
+	draw_line(vertices[-1],vertices[0],Color(0.0,0.0,0.0),2.0,true)
+	
+
+# Returns the center of mass, aka centroid, of the polygon in local coords
+func center_of_mass() -> Vector2:
+	var x : float = 0
+	var y : float = 0
+	for i in range(len(vertices)):
+		var vi = vertices[i]
+		var vi1 = vertices[(i+1)%len(vertices)]
+		x += (vi.x + vi1.x)*(vi.x * vi1.y - vi1.x * vi.y)
+		y += (vi.y + vi1.y)*(vi.x * vi1.y - vi1.x * vi.y) 
+	return 1/(6 * calculate_area()) * Vector2(x,y)
+
+
+# Returns the global coords of the vertex that is leftmost in global coords
+func leftmost_vertex() -> Vector2:
+	var xs = []
+	for v in vertices:
+		xs.push_back(get_global_transform().xform(v).x)
+	var argmin = xs.find(xs.min())
+	return get_global_transform().xform(vertices[argmin])
+
+
+# Returns the global coords of the vertex that is rightmost in global coords
+func rightmost_vertex() -> Vector2:
+	var xs = []
+	for v in vertices:
+		xs.push_back(get_global_transform().xform(v).x)
+	var argmax = xs.find(xs.max())
+	return get_global_transform().xform(vertices[argmax])
+
