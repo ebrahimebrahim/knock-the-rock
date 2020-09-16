4d9bc8c09b9227e1b4cf5e7000f08d568507b83c
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-16 14:31:37 -0400

    set title text to match rock tint

--- ktr/Logo.gd
+++ ktr/Logo.gd
@@ -8,3 +8,4 @@ func _ready():
 	logo_polygon = LogoPolygon.new(abs($xRadius.position.x),abs($yRadius.position.y))
 	add_child(logo_polygon)
 	move_child(logo_polygon,0) # Move to top to get drawn underneath oher stuff
+	$Title.modulate = logo_polygon.self_modulate # set title text to match rock tint 
