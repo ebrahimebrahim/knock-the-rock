a1ce6fa54d6180df71f311fe76b02e9a4539192c
Yusuf Ebrahim <yusufmte@gmail.com>
2020-09-17 12:30:25 -0400

    moved boulder spawning code into GameBase along with the BoulderBotRight line, since Challenge mode will likely spawn a boulder in the same way
    Thinking we should do the same for rock spawning code next, although the rock spawning line would be in a different position, and it would be a different number of rocks that are spawned.

--- ktr/GameBase.gd
+++ ktr/GameBase.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const Boulder = preload("res://Boulder.gd")
+
 var scrolling_bg : bool = false
 var change_scene_to : String
 
@@ -8,11 +10,17 @@ var closed_hand = preload("res://images/closed_hand.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	randomize()
+	
 	$HUDZone/ReturnButton.connect("button_up",self,"_on_return")
 	$HUDZone/RestartButton.connect("button_up",self,"_on_restart")
 	$HUDZone/HelpButton.connect("button_up",self,"_on_help")
 	$HelpOverlay/Help.connect("hide_help",self,"_on_help")
 	Input.set_custom_mouse_cursor(open_hand,Input.CURSOR_ARROW,Vector2(21,27))
+	
+	var beuld : Boulder = Boulder.new()
+	add_child(beuld)
+	beuld.set_position(random_point_on_line($BoulderBotRight)-beuld.bottom_right())
 
 func _input(event):
 	if event.is_action_pressed("return_to_menu"): _on_return()
@@ -20,6 +28,10 @@ func _input(event):
 	if event.is_action_pressed("open_help"): _on_help()
 	if event.is_action_pressed("toggle_hud"): _on_toggle()
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT: update_cursor()
+
+func random_point_on_line(line : Line2D):
+	assert(len(line.points)==2)
+	return line.points[0]+randf()*(line.points[1] - line.points[0])
 
 func _on_return():
 	if scrolling_bg : get_tree().quit() # allows double tap esc to quit
--- ktr/GameSandbox.gd
+++ ktr/GameSandbox.gd
@@ -1,18 +1,8 @@
 extends "res://GameBase.gd"
 
 const Rock = preload("res://Rock.gd")
-const Boulder = preload("res://Boulder.gd")
-
-func random_point_on_line(line : Line2D):
-	assert(len(line.points)==2)
-	return line.points[0]+randf()*(line.points[1] - line.points[0])
 
 func _ready():
-	randomize()
-	var beuld : Boulder = Boulder.new()
-	add_child(beuld)
-	beuld.set_position(random_point_on_line($BoulderBotRight)-beuld.bottom_right())
-
 	var rocks = []
 	for _i in range(2):
 		var rock : Rock = Rock.new()
@@ -37,4 +27,3 @@ func _input(event):
 		add_child(rock)
 		rock.flat_bottom()
 		rock.position = rock.global_transform.xform(-rock.center_of_mass() + rock.global_transform.xform_inv(event.position))
-
