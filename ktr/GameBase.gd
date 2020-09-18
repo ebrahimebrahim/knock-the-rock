a0c51587f4770c99e6ba3444996112c77de53b8c
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-18 01:15:27 -0400

    fix random_point_on_line offset

--- ktr/GameBase.gd
+++ ktr/GameBase.gd
@@ -49,9 +49,10 @@ func spawn_rocks(num_rocks : int, spawn_line : Line2D):
 				break
 		rocks.append(rock)
 
+# returns random point on given line, in global coords
 func random_point_on_line(line : Line2D):
 	assert(len(line.points)==2)
-	return (line.points[0]+randf()*(line.points[1] - line.points[0]))-line.global_position
+	return (line.points[0]+randf()*(line.points[1] - line.points[0])) + line.global_position
 
 func _on_return():
 	if scrolling_bg : get_tree().quit() # allows double tap esc to quit
