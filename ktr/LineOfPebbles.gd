38aadf3be8a4649fa1f8bdd5f140d6890a547e0a
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-18 19:10:21 -0400

    disable rocks that get out of the throw zone

--- ktr/LineOfPebbles.gd
+++ ktr/LineOfPebbles.gd
@@ -1,6 +1,7 @@
 extends Line2D
 
 const RockPolyon = preload("RockPolygon.gd")
+const Rock = preload("Rock.gd")
 
 const pebble_scale = 0.1
 
@@ -8,6 +9,8 @@ var num_pebbles : int
 
 
 func _ready():
+	self_modulate = Color(0,0,0,0) # Make line not visible w/o hiding child pebbles
+	
 	assert(len(points)==2)
 	var line_length = (points[1]-points[0]).length()
 	num_pebbles = 3 + randi()%3
@@ -16,3 +19,9 @@ func _ready():
 		pebble_polygon.scale = pebble_scale * Vector2(1,1)
 		add_child(pebble_polygon)
 		pebble_polygon.position = points[0] + float(i)/(num_pebbles-1) * (points[1]-points[0])
+
+
+func _on_ThrowZone_body_exited(body):
+	if body is Rock:
+		body.holdable=false
+		body.set_held(false)
