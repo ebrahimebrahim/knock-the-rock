383cdb6fe00071adcf33637fc7cd85b541e8b3ce
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-18 17:41:04 -0400

    don't lock out throwing spawning rocks in chlg mode
    Instead let player throw, and also keep retrying to spawn rocks
    till one succeeds.
    
    Basically just spawn a rock on the boulder whenever the previous target
    rock has been missing for long enough.
    
    And be aware of whether or not a throwing rock has touched the target rock before.
    If one has, then when that target rock falls we can count it as a point
    (regardless of whether the fall was due to poor initial placement)

--- ktr/GameChallenge.gd
+++ ktr/GameChallenge.gd
@@ -1,8 +1,10 @@
 extends "res://GameBase.gd"
 
 var target_rock : Rock
+var target_rock_has_been_touched : bool
 var throwing_rocks = [] # list of Rocks that can be thrown-- some items in list will have been deleted at times
 var beuld_topmid : Vector2
+
 
 func _ready():
 	for spawn_line in $RockSpawnLines.get_children():
@@ -14,36 +16,29 @@ func _ready():
 
 
 func _process(delta):
-	if not target_rock_on_boulder() and $Timers/DelayTillSpawnTarget.is_stopped():
-		$Timers/DelayTillSpawnTarget.start()
-		print("started tiemr!")
+	if scrolling_bg : return
+	if not target_rock_on_boulder() and $DelayTillSpawnTarget.is_stopped():
+		$DelayTillSpawnTarget.start()
 
 
 func target_rock_on_boulder() -> bool:
 	return is_instance_valid(target_rock) and target_rock.position.y < beuld_topmid.y
 
 
-func set_all_throwing_holdable(holdable : bool):
-	for rock in throwing_rocks:
-		if is_instance_valid(rock):
-			rock.holdable = holdable
-
 func place_new_target_rock():
-	spawn_new_target_rock()
-	set_all_throwing_holdable(false)
-	$Timers/TargetRockPlacement.start()
-
-
-func spawn_new_target_rock():
 	target_rock = Rock.new()
 	add_child(target_rock)
 	target_rock.position += beuld_topmid-target_rock.global_transform.xform(target_rock.center_of_mass())+Vector2(0,-20-target_rock.flat_bottom())
 	target_rock.holdable = false
+	target_rock.connect("body_entered",self,"_on_target_rock_contact")
+	target_rock_has_been_touched = false
 
 
 func _on_TargetRockPlacementTimer_timeout():
-	if target_rock_on_boulder():
-		set_all_throwing_holdable(true)
-	else:
-		place_new_target_rock()
+	place_new_target_rock()
 		
+
+func _on_target_rock_contact(body):
+	if body is Boulder or not (body is Rock): return # We only care about non-boulder rocks
+	if body in throwing_rocks:
+		target_rock_has_been_touched = true
