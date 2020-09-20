b7fc3a38af8905567a1b1e6f0e007732c0af1ca6
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-20 19:06:24 -0400

    enforce providing reason for nonholdability
    _holdable is now private to Rock
    set_holdable will throw a fit if you set to false without giving a
    reason

--- ktr/LineOfPebbles.gd
+++ ktr/LineOfPebbles.gd
@@ -25,7 +25,7 @@ func _ready():
 
 func _on_ThrowZone_body_exited(body):
 	if body is Rock:
-		if body.holdable:
+		if body.is_holdable():
 			body.set_holdable(false,"No picking up rocks beyond the line of pebbles!")
 			emit_signal("rock_lost",body)
 		if body.is_held:
@@ -34,6 +34,6 @@ func _on_ThrowZone_body_exited(body):
 
 func _on_ThrowZone_body_entered(body):
 	if body is Rock:
-		if not body.holdable:
+		if not body.is_holdable():
 			body.set_holdable(true)
 			emit_signal("rock_regained",body)
