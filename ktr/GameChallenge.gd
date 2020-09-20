ebrahimebrahim/knock-the-rock

commit 5723ce248ee3f73fb17f18f34ee6192dd0d377ac

add delay before new throwing rock are spawned in
i felt pretty makhboos having the rock i just threw get replaced instantly

ktr/GameChallenge.gd
@@ -84,7 +84,11 @@ func _on_LineOfPebbles_rock_lost(body):
 	change_throwing_rocks_remaining(-1)
 	holdable_throwing_rocks.erase(body)
 	if len(holdable_throwing_rocks) < 2 and throwing_rocks_remaining > len(holdable_throwing_rocks):
-		place_new_throwing_rocks(1)
+		$DelayTillReplaceThrowingRocks.start()
+
+
+func _on_DelayTillReplaceThrowingRocks_timeout():
+	place_new_throwing_rocks(1)
 
 
 func _on_LineOfPebbles_rock_regained(body):
@@ -103,3 +107,4 @@ func _on_ThrowZone_mouse_exited():
 func _on_ThrowZone_mouse_entered():
 	clickable = true
 	Input.set_custom_mouse_cursor(open_hand,Input.CURSOR_ARROW,Vector2(21,27))
+

ktr/GameChallenge.tscn
@@ -68,7 +68,13 @@ __meta__ = {
 "_editor_description_": "How long to wait after target rock is knocked off before spawning a new target rock"
 }
 
-[node name="DelayTillEndGame" type="Timer" parent="." index="9"]
+[node name="DelayTillReplaceThrowingRocks" type="Timer" parent="." index="9"]
+one_shot = true
+__meta__ = {
+"_editor_description_": "How long to wait after a new throwing rock is needed before providing it."
+}
+
+[node name="DelayTillEndGame" type="Timer" parent="." index="10"]
 wait_time = 2.0
 one_shot = true
 __meta__ = {
@@ -81,4 +87,5 @@ __meta__ = {
 [connection signal="mouse_entered" from="LineOfPebbles/ThrowZone" to="." method="_on_ThrowZone_mouse_entered"]
 [connection signal="mouse_exited" from="LineOfPebbles/ThrowZone" to="." method="_on_ThrowZone_mouse_exited"]
 [connection signal="timeout" from="DelayTillSpawnTarget" to="." method="place_new_target_rock"]
+[connection signal="timeout" from="DelayTillReplaceThrowingRocks" to="." method="_on_DelayTillReplaceThrowingRocks_timeout"]
 [connection signal="timeout" from="DelayTillEndGame" to="." method="_on_DelayTillEndGame_timeout"]

commit 5584ad1e64880832047f3dfd7354147b9a6d5dd8

change delay rules for throwable rock spawning
This fixes an issue I introduced in 5723ce248ee3f73fb17f18f34ee6192dd0d377ac
The issue is that there was just one delay timer managing the spawning of up
to two throwable rocks. Sometimes the timer gets restarted twice, and then
triggers the spawning of only one rock.

Now the setup is this:
When a throwable rock gets lost, we start a timer to replace it.
If a second throwable rock is lost that needs to be replaced, then
that one is just replaced instantly.

ktr/GameChallenge.gd
@@ -84,7 +84,10 @@ func _on_LineOfPebbles_rock_lost(body):
 	change_throwing_rocks_remaining(-1)
 	holdable_throwing_rocks.erase(body)
 	if len(holdable_throwing_rocks) < 2 and throwing_rocks_remaining > len(holdable_throwing_rocks):
-		$DelayTillReplaceThrowingRocks.start()
+		if len(holdable_throwing_rocks) == 0:
+			place_new_throwing_rocks(1) # if no rocks t throw, provide one immediately
+		else: # but if there's one rock to throw already, provide the second with delay
+			$DelayTillReplaceThrowingRocks.start()
 
 
 func _on_DelayTillReplaceThrowingRocks_timeout():