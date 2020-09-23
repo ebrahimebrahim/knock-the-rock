acdf25254834429d9a6967d7baec2d1edf078b85
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-23 08:13:19 -0400

    add exported vars for all the new settings

--- ktr/SettingsConfig.gd
+++ ktr/SettingsConfig.gd
@@ -4,3 +4,7 @@ class_name SettingsConfig
 
 export var fullscreen : bool
 export var music_vol : float
+export var gravity : float
+export var challenge_mode_rocks : int
+export var corner_menu_hidden_by_default : bool
+export var language : String
