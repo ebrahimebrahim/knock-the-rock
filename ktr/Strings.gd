6df1ecd5880a0ae070789385ef158e1df1b6020a
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-23 11:33:12 -0400

    clean up endgame message
    this is part of #18

--- ktr/Strings.gd
+++ ktr/Strings.gd
@@ -86,7 +86,7 @@ func throwing_remaining(t):
 
 func endgame_message(score, total_rocks_given):
 	var msg_index = min(int((float(score)/total_rocks_given)*(len(_endgame_messages[lang])-1)),len(_endgame_messages[lang])-1)
-	return _score[lang] + ": " + str(score) + "\n\""+ _endgame_messages[lang][msg_index] + "\"\n" + _to_proceed[lang]
+	return _score[lang] + ": " + str(score) + "\n"+ _endgame_messages[lang][msg_index] 
 
 func mistake_message(mistakes_made):
 	return _mistake_messages[lang][mistakes_made%len(_mistake_messages[lang])]
