aa1be1b5435fcdc62403c33e953c79d25ef9eba1
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-26 06:49:51 -0400

    attempting deal with issue 29
    Starting by separating the "stopped" and the "deleted" parts of
    "stopped or deleted" signal mechanism

--- ktr/GameChallenge.gd
+++ ktr/GameChallenge.gd
@@ -2,6 +2,7 @@ extends "res://GameBase.gd"
 
 var game_has_ended = false # true when game is for sure over
 var game_might_end = false # true when last rock was thrown and we are waiting to see if player somehow gets it back or something
+var last_rock_thrown : Rock # if game_might_end, this is useful
 
 var target_rock : Rock
 var target_rock_has_been_touched : bool
@@ -169,24 +170,16 @@ func _on_target_rock_contact(body):
 		target_rock_has_been_touched = true
 
 
-func _on_DelayTillEndGame_timeout():
-	if throwing_rocks_remaining <= 0 and not game_has_ended:
-		game_has_ended = true
-		$EndgameRufflePlayer.play()
-		show_message(Strings.endgame_message(score,total_rocks_given),-1)
-	elif throwing_rocks_remaining > 0:
-		game_might_end = false
-
-
-
 func _on_LineOfPebbles_rock_lost(rock : Rock):
 	if scene_shutting_down or game_has_ended: return
 	change_throwing_rocks_remaining(-1)
 	if throwing_rocks_remaining <= 0:
 		# initiate possible endgame sequence
-		rock.monitor_stopped_or_deleted = true
+		rock.monitor_stopped = true
 		game_might_end = true
-		rock.connect("stopped_or_deleted",self,"_last_rock_stopped_or_gone",[rock],CONNECT_ONESHOT) 
+		last_rock_thrown = rock
+		rock.connect("stopped",self,"_last_rock_stopped_or_gone",[rock],CONNECT_ONESHOT)
+		rock.connect("tree_exiting",self,"_last_rock_stopped_or_gone",[rock],CONNECT_ONESHOT | CONNECT_DEFERRED)
 	throwzone_rocks.erase(rock)
 	var num_throwzone_rocks_including_incoming : int = len(throwzone_rocks) + (0 if $DelayTillReplaceThrowingRocks.is_stopped() else 1)
 	if len(throwzone_rocks) < 2 and throwing_rocks_remaining > num_throwzone_rocks_including_incoming:
@@ -201,8 +194,21 @@ func _on_DelayTillReplaceThrowingRocks_timeout():
 
 
 func _last_rock_stopped_or_gone(rock : Rock) -> void:
-	rock.monitor_stopped_or_deleted = false
+	rock.monitor_stopped = false
+	if rock.is_connected("stopped",self,"_last_rock_stopped_or_gone"):
+		rock.disconnect("stopped",self,"_last_rock_stopped_or_gone")
+	if rock.is_connected("tree_exiting",self,"_last_rock_stopped_or_gone"):
+		rock.disconnect("tree_exiting",self,"_last_rock_stopped_or_gone")
 	$DelayTillEndGame.start()
+
+
+func _on_DelayTillEndGame_timeout():
+	if throwing_rocks_remaining <= 0 and not game_has_ended:
+		game_has_ended = true
+		$EndgameRufflePlayer.play()
+		show_message(Strings.endgame_message(score,total_rocks_given),-1)
+	elif throwing_rocks_remaining > 0:
+		game_might_end = false
 
 
 func _on_LineOfPebbles_rock_regained(rock : Rock):
