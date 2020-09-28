65ffedd428f52596bceb285a038ab5ff41367bbd
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-09-28 12:25:04 -0400

    create special mode
    night time with fireflies

---
+++ ktr/GameChallengeSpecial.gd
@@ -0,0 +1,20 @@
+extends "res://GameChallenge.gd"
+
+const rock_modulate = Color(0.4,0.48,0.72)
+
+func _ready():
+	var x_min : float  = $bg/FirefliesRectTopLeft.position.x
+	var x_max : float  = $bg/FirefliesRectBotRight.position.x
+	var y_min : float  = $bg/FirefliesRectTopLeft.position.y
+	var y_max : float  = $bg/FirefliesRectBotRight.position.y
+	$bg/Fireflies.position = Vector2(
+		rand_range(x_min,x_max), 
+		rand_range(y_min,y_max)
+	)
+
+	$bg/Fireflies.play()
+	
+	beuld.modulate = rock_modulate
+
+	$LineOfPebbles.modulate = rock_modulate
+	$RockList.modulate = rock_modulate
