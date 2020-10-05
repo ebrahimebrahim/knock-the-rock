75f4543a4b97d1f759053a49db26fdcf54c7ac37
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2020-10-04 21:40:23 -0400

    unset autowrap for messages
    Anchors need to be used if we want it to work properly:
    https://godotengine.org/qa/42760/label-autowrap-makes-line-breaks-at-every-space
    
    I'm going to go with no autowrap-- it might be easier to manually put in
    line breaks actually. It might be preferred

--- ktr/Message.gd
+++ ktr/Message.gd
@@ -5,13 +5,14 @@ var message_lifetime : float
 
 func _init(msg : String, time : float = 4):
 	set_theme(preload("message_theme.tres"))
-	set_autowrap(true)
+	# set_autowrap(true)  # If you turn this on then you need to set left and right anchors
 	set_mouse_filter(MOUSE_FILTER_IGNORE)
 	
 	text = msg
 	message_lifetime = time
 	rect_position = Vector2(200,200)
-	rect_size = Vector2(739,175)
+	rect_size = Vector2(739,800)
+	print(margin_left," ",margin_right)
 
 
 func _ready():
